a93ceccda1c433db7c4de02d234fd187ffc95928
Deepa <deepanshupatel9229@gmail.com>
2025-07-12 23:29:25 +0530

    techStack added

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import {BallCanvas} from "./canvas"
 import { SectionWrapper } from '../hoc'
 import { technologies } from '../constants'
+import { styles } from '../styles'
+import { textVariant } from '../utils/motion'
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-6 sm:gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-20 h-20 sm:w-28 sm:h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} name={technology.name} />
-        </div>
-      ))}
-    </div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText}`}>My skills</p>
+        <h2 className={`${styles.sectionHeadText}`}>Tech Stack.</h2>
+      </motion.div>
+      
+      <div className='flex flex-row flex-wrap justify-center gap-6 sm:gap-10 mt-10'>
+        {technologies.map((technology) => (
+          <div className='w-20 h-20 sm:w-28 sm:h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} name={technology.name} />
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
